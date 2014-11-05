b551a048f8ce07cb40fa236baeea1f9a2e186465
Alan W. Smith <alan@alanwsmith.com>
2014-11-05 16:22:42 -0500

    Hamming class deals with strings of different sizes.

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,9 +2,17 @@ class Hamming
 
   def self.compute(reference, examinee)
 
+    if reference.length > examinee.length
+      run_length = examinee.length
+    else
+      run_length = reference.length
+    end
+
+    run_length -= 1
+
     hamming_distance = 0
 
-    (0..reference.length).each do |string_index|
+    (0..run_length).each do |string_index|
       if examinee[string_index] != reference[string_index]
         hamming_distance += 1
       end
